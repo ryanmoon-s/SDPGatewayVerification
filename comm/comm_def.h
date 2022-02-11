df3aee111940a968333aad2e4297ed730f6a9ff7
shun <1119067989@qq.com>
2022-02-11 17:29:17 +0800

    add tools comm dir

---
+++ comm/comm_def.h
@@ -0,0 +1,21 @@
+/* ************************************************************************
+> File Name:     comm_def.h
+> Author:        arashi
+> mail:          arashi@tencent.com
+> Created Time:  2022年02月11日 星期五 17时07分14秒
+> Description:   
+ ************************************************************************/
+
+#ifndef COMM_DEF_H
+#define COMM_DEF_H
+
+#define LOCAL_IP 127.0.0.1
+#define SPA_PORT 6660
+#define TLS_PORT 6661
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+
+#endif
